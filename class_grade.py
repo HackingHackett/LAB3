7ceaa81480ff44de3d76e59816100a10c3f43f62
hack <hack>
2020-12-18 00:19:53 -0800

    Loop

--- class_grade.py
+++ class_grade.py
@@ -1,16 +1,14 @@
 
-"""Plain English"""
+""""""Plain English"""
 """ 
-Start
+"""Start
 Create a list to store 5 number (float)
 capture users input 5 times for their grades
 each time we capture the suers input we append the number to the list 
 sort the list ascending then splice the items statring at index 2
 once we have three highest number in the list we sum them up and divide by 3
 output a message to user
-end
-
-"""
+end"""
 
 """ psudo code
 create list
@@ -31,8 +29,6 @@ append number to list
 
 sort the list then splice out the two lowest number
 print message to user
-
-"""
 
 grades = []
 
@@ -59,4 +55,17 @@ grades = sum(grades)
 result = grades / 3
 
 print("Average Grade {0:.2f}%".format(result))
-print (grades, 'results', result)
+print (grades, 'results', result)"""
+
+"""
+CODE REFACTOR USING LOOP
+"""
+grades = []
+
+for i in range(5):
+    grades.append(float(input("Enter the grade: ")))
+
+    grades.sort()
+    grades = sum(grades[2:]) / 3
+    
+    print ("Average Grade {0:.2f}%".format(grades))
